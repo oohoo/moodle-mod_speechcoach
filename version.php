b6915a94bf43821894708c922fc73b411a5c1baa
Andrew McCann <amccann@ualberta.ca>
2012-08-15 11:30:46 -0600

    Made insignificant changes required for release.

--- version.php
+++ version.php
@@ -1,21 +1,4 @@
 <?php
-
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-
 /**
  * Defines the version of speechcoach
  *
@@ -30,7 +13,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2012062100;      // The current module version (Date: YYYYMMDDXX)
-//$module->requires  = 2010031900;      // Requires this Moodle version
+$plugin->version = 2012081500;
+$plugin->requires = 2010031900;
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '1.0.0';
+$module->component = 'mod_speechcoach';
 $module->cron      = 0;               // Period for cron to check this module (secs)
-$module->component = 'mod_speechcoach'; // To check on upgrade, that module sits in correct place
