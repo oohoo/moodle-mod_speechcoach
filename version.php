860ce00ae551f923d78b1b2c6205af54195d7f35
Andrew McCann <amccann@ualberta.ca>
2012-08-15 14:27:07 -0600

    Changed from RC to STABLE

--- version.php
+++ version.php
@@ -15,7 +15,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2012081500;
 $plugin->requires = 2010031900;
-$plugin->maturity = MATURITY_RC;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
 $module->component = 'mod_speechcoach';
 $module->cron      = 0;               // Period for cron to check this module (secs)
